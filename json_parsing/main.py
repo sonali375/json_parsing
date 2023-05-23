8d914513042e4536224b947acc89e2a23a3b6031
Sonali Sahu <71460151+sonali375@users.noreply.github.com>
2023-05-23 10:32:00 +0530

    Add files via upload

--- json_parsing/main.py
+++ json_parsing/main.py
@@ -2,21 +2,20 @@
 import json
 
 # Opening JSON file
-f = open('input.json', "r")
+file = open('input.json', "r")
 
-# returns JSON object as
-# a dictionary
-data = json.load(f)
+# returns JSON object as a dictionary
+data = json.load(file)
 
 # Iterating through the json
 # list
 sonali_list = []
-for i in data['parametersList']:
+for item in data['parametersList']:
     my_dict = {
-        "parameterName": i['parameterName'],
-        "min": i['min'],
-        "max": i['max'],
-        "avg": i['avg']
+        "parameterName": item['parameterName'],
+        "min": item['min'],
+        "max": item['max'],
+        "avg": item['avg']
 
     }
 
@@ -25,9 +24,19 @@ my_list = json.dumps(sonali_list)
 # dumps helps to convert it into json format so it will return a string
 print(my_list)
 
-with open("input.json", "w") as file1:
-    file1.write(my_list)
-print(sonali_list)
+# with open("input.json", "w") as file1:
+#     file1.write(my_list)
+# print(sonali_list)
+
+for each_keys in data:
+    # print(each_keys)
+    # print(json_data[each_keys])
+    if isinstance(data[each_keys], list):
+        print(each_keys)
+        print(type(data[each_keys]))
+        print()
+        for each_items in data[each_keys]:
+            print(each_items)
 
 # Closing file
-f.close()
+file.close()
