64819d68715276b1a817413d5e808e2f75c71297
Sonali Sahu <71460151+sonali375@users.noreply.github.com>
2023-05-23 18:02:41 +0530

    Add files via upload

--- json_parsing/main.py
+++ json_parsing/main.py
@@ -24,19 +24,19 @@ my_list = json.dumps(sonali_list)
 # dumps helps to convert it into json format so it will return a string
 print(my_list)
 
-# with open("input.json", "w") as file1:
-#     file1.write(my_list)
-# print(sonali_list)
+with open("output.json", "w") as file1:
+    file1.write(my_list)
+print(sonali_list)
 
-for each_keys in data:
-    # print(each_keys)
-    # print(json_data[each_keys])
-    if isinstance(data[each_keys], list):
-        print(each_keys)
-        print(type(data[each_keys]))
-        print()
-        for each_items in data[each_keys]:
-            print(each_items)
+# for each_keys in data:
+#     # print(each_keys)
+#     # print(json_data[each_keys])
+#     if isinstance(data[each_keys], list):
+#         print(each_keys)
+#         print(type(data[each_keys]))
+#         print()
+#         for each_items in data[each_keys]:
+#             print(each_items)
 
 # Closing file
 file.close()
